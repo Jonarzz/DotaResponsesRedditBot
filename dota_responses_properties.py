591870cd4288fa704f6d6d210578d17959928731
Jonasz Czerepko <jonasz.czerepko@gmail.com>
2015-10-05 16:06:08 +0200

    Version 1.5 - see the changelog for further information.

--- dota_responses_properties.py
+++ dota_responses_properties.py
@@ -8,7 +8,9 @@ USER_AGENT = """A tool that finds a Dota 2-related comments with the game heroes
              audio sample from http://dota2.gamepedia.com/Category:Lists_of_responses (author: /u/Jonarz)"""
 SUBREDDIT = "dota2"
 SCOPES = "identity read save submit"
+
 COMMENT_ENDING = "\n\n---\n*I am a bot. Question/problem? Ask my master: /u/Jonarz*\n\n*Description/changelog:* [GitHub](https://github.com/Jonarzz/DotaResponsesRedditBot)"
+
 EXCLUDED_RESPONSES = ["thank you", "why not?", "glimmer cape", "hood of defiance", "mask of madness", "force staff", "armlet of mordiggian", "helm of the dominator", "veil of discord",
                       "shadow blade", "blade mail", "urn of shadows", "skull basher", "battle fury", "crimson guard", "eul's scepter", "eul's scepter of divinity", "scepter of divinity",
                       "ethereal blade", "black king bar", "diffusal blade", "lotus orb", "silver edge", "solar crest", "medallion of courage", "rod of atos", "shiva's guard",
@@ -19,4 +21,14 @@ EXCLUDED_RESPONSES = ["thank you", "why not?", "glimmer cape", "hood of defiance
                       "elder titan", "ember spirit", "faceless void", "keeper of the light", "legion commander", "lone druid", "naga siren", "nature's prophet", "natures prophet",
                       "night stalker", "nyx assassin", "ogre magi", "outworld destroyer", "phantom assassin", "phantom lancer", "queen of pain", "sand king", "shadow demon", "shadow fiend",
                       "skywrath mage", "skeleton king", "spirit breaker", "storm spirit", "templar assassin", "treant protector", "troll warlord", "vengeful spirit", "winter wyvern",
-                      "witch doctor", "wraith king", "i agree", "my bad", "ha ha", "why not", "fair enough", "no way"]
+                      "witch doctor", "wraith king", "i agree", "my bad", "ha ha", "why not", "fair enough", "no way", "you're welcome", "very nice", "of course", "well deserved"]
+
+KEYWORDS_DICT = {"dank" : "http://hydra-media.cursecdn.com/dota2.gamepedia.com/3/38/Erth_ability_echo_03.mp3",
+                 "just do it" : "http://hydra-media.cursecdn.com/dota2.gamepedia.com/c/c4/Sven_move_07.mp3",
+                 "beautiful": "http://hydra-media.cursecdn.com/dota2.gamepedia.com/c/c4/Luna_levelup_06.mp3",
+                 "from the ghastly eyrie" : "http://hydra-media.cursecdn.com/dota2.gamepedia.com/a/ac/Pain_lose_04.mp3",
+                 "sniper" : " http://hydra-media.cursecdn.com/dota2.gamepedia.com/1/17/Snip_ability_shrapnel_03.mp3",
+                 "ppd" : "http://hydra-media.cursecdn.com/dota2.gamepedia.com/4/47/Necr_death_10.mp3",
+                 "leafeator" : "http://hydra-media.cursecdn.com/dota2.gamepedia.com/e/e5/Necr_respawn_10.mp3",
+                 "ur brother" : "http://hydra-media.cursecdn.com/dota2.gamepedia.com/6/63/Ogmag_respawn_05.mp3",
+                 "your brother" : "http://hydra-media.cursecdn.com/dota2.gamepedia.com/6/63/Ogmag_respawn_05.mp3"}
--- dotaresponses.py
+++ dotaresponses.py
@@ -11,10 +11,14 @@ def execute():
     r = account.get_account()
     responses_dict = parser.dictionary_from_file("dota_responses_1.2.txt")
     already_done_comments = load_already_done_comments()
-    already_done_submissions = load_already_done_submissions()
 
-    # DELETE
-    submission = r.get_submission(submission_id="3ncgea")
+    for submission in r.get_subreddit(properties.SUBREDDIT).get_new(limit=100):
+        add_comments(submission, already_done_comments, responses_dict)
+    for submission in r.get_subreddit(properties.SUBREDDIT).get_hot(limit=25):
+        add_comments(submission, already_done_comments, responses_dict)
+
+
+def add_comments(submission, already_done_comments, responses_dict):
     submission.replace_more_comments(limit=None, threshold=0)
 
     for comment in praw.helpers.flatten_tree(submission.comments):
@@ -23,35 +27,19 @@ def execute():
         already_done_comments.append(comment.id)
 
         comment_text = prepare_comment(comment.body)
+
+        for word in properties.KEYWORDS_DICT:
+            if word in comment_text:
+                comment.reply(properties.KEYWORDS_DICT[word] + properties.COMMENT_ENDING)
+                print("Added: " + comment.id)
+                break
+
         for key in responses_dict:
             if comment_text == key:
-                comment.reply(responses_dict[key] + properties.COMMENT_ENDING)
-                print("Added: " + comment.id)
-
-                break
-    # DELETE
-
-    comment_new(r, already_done_comments, responses_dict)
-    comment_hot(r, already_done_comments, already_done_submissions, responses_dict)
-
-
-def comment_new(r, already_done_comments, responses_dict):
-    for submission in r.get_subreddit(properties.SUBREDDIT).get_new(limit=100):
-        submission.replace_more_comments(limit=None, threshold=0)
-
-        for comment in praw.helpers.flatten_tree(submission.comments):
-            if comment.id in already_done_comments:
-                continue
-            already_done_comments.append(comment.id)
-
-            comment_text = prepare_comment(comment.body)
-            for key in responses_dict:
-                if comment_text == key:
-                    if comment_text not in properties.EXCLUDED_RESPONSES:
-                        comment.reply(responses_dict[key] + properties.COMMENT_ENDING)
-                        print("Added: " + comment.id)
-
-                        break
+                if comment_text not in properties.EXCLUDED_RESPONSES:
+                    comment.reply(responses_dict[key] + properties.COMMENT_ENDING)
+                    print("Added: " + comment.id)
+                    break
 
     save_already_done_comments(already_done_comments)
 
@@ -66,44 +54,6 @@ def load_already_done_comments():
     with open("already_done_comments.txt") as file:
         already_done_comments = [i for i in file.read().split()]
         return already_done_comments
-
-
-def comment_hot(r, already_done_comments, already_done_submissions, responses_dict):
-    for submission in r.get_subreddit(properties.SUBREDDIT).get_hot(limit=25):
-        # if submission.id in already_done_submissions:
-        #         continue
-        # already_done_submissions.append(submission.id)
-
-        submission.replace_more_comments(limit=None, threshold=0)
-
-        for comment in praw.helpers.flatten_tree(submission.comments):
-            if comment.id in already_done_comments:
-                continue
-            already_done_comments.append(comment.id)
-
-            comment_text = prepare_comment(comment.body)
-            for key in responses_dict:
-                if comment_text == key:
-                    if comment_text not in properties.EXCLUDED_RESPONSES:
-                        comment.reply(responses_dict[key] + properties.COMMENT_ENDING)
-                        print("Added: " + comment.id)
-
-                        break
-
-    # save_already_done_submissions(already_done_submissions)
-    save_already_done_comments(already_done_comments)
-
-
-def save_already_done_submissions(already_done_submissions):
-    with open("already_done_submissions.txt", "w") as file:
-        for item in already_done_submissions:
-            file.write("%s " % item)
-
-
-def load_already_done_submissions():
-    with open("already_done_submissions.txt") as file:
-        already_done_submissions = [i for i in file.read().split()]
-        return already_done_submissions
 
 
 def prepare_comment(comment):
