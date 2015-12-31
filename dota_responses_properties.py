ac4dd03ebe41fac37630022295c8f9d75a66b8e4
Jonasz Czerepko <jonasz.czerepko@gmail.com>
2015-12-31 15:43:00 +0100

    Added a file with properties (mostly empty values for safety).

---
+++ dota_responses_properties.py
@@ -0,0 +1,55 @@
+"""Module in which the constants that are used by Dota Responses Bot are declared."""
+
+__author__ = 'Jonarzz'
+
+APP_ID = ''
+APP_SECRET = ''
+APP_URI = ''
+APP_REFRESH_CODE = ''
+USER_AGENT = """A tool that finds a Dota 2-related comments with the game heroes\' responses
+                and links to the proper audio sample from
+                http://dota2.gamepedia.com/Category:Lists_of_responses (author: /u/Jonarz)"""
+SUBREDDIT = 'dota2'
+SCOPES = ''
+RESPONSES_FILENAME = ''
+HEROES_FILENAME = ''
+
+COMMENT_ENDING = ''
+                    
+INFO_FILENAME = ''
+ERROR_FILENAME = ''
+
+EXCLUDED_RESPONSES = ["thank you", "why not?", "glimmer cape", "hood of defiance",
+                      "mask of madness", "force staff", "armlet of mordiggian",
+                      "helm of the dominator", "veil of discord", "shadow blade", "blade mail",
+                      "urn of shadows", "skull basher", "battle fury", "crimson guard",
+                      "eul's scepter", "eul's scepter of divinity", "scepter of divinity",
+                      "ethereal blade", "black king bar", "diffusal blade", "lotus orb",
+                      "silver edge", "solar crest", "medallion of courage", "rod of atos",
+                      "shiva's guard", "heaven's halberd", "sange and yasha", "monkey king bar",
+                      "orchid malevolence", "drum of endurance", "aghanim's scepter",
+                      "manta style", "eye of skadi", "hand of midas", "vladimir's offering",
+                      "refresher orb", "linken's sphere", "assault cuirass", "divine rapier",
+                      "scythe of vyse", "sheep stick", "pipe of insight", "boots of travel",
+                      "blink dagger", "moon shard", "guardian greaves", "octarine core",
+                      "heart of tarrasque", "abyssal blade", "abyssal underlord",
+                      "ancient apparition", "anti mage", "bounty hunter", "centaur warrunner",
+                      "chaos knight", "crystal maiden", "dark seer", "death prophet",
+                      "dragon knight", "drow ranger", "earth spirit", "earth shaker",
+                      "elder titan", "ember spirit", "faceless void", "keeper of the light",
+                      "legion commander", "lone druid", "naga siren", "nature's prophet",
+                      "natures prophet", "night stalker", "nyx assassin", "ogre magi",
+                      "outworld destroyer", "phantom assassin", "phantom lancer", "queen of pain",
+                      "sand king", "shadow demon", "shadow fiend", "skywrath mage",
+                      "skeleton king", "spirit breaker", "storm spirit", "templar assassin",
+                      "treant protector", "troll warlord", "vengeful spirit", "winter wyvern",
+                      "witch doctor", "wraith king", "i agree", "my bad", "ha ha", "why not",
+                      "fair enough", "no way", "you're welcome", "very nice", "of course",
+                      "well deserved", "try again", "it worked", "nice try", "seems fair",
+                      "that's right", "thank god", "thank you so much", "well said", "holy shit",
+                      "so beautiful", "try harder", "go outside", "arc warden", "he he he",
+                      "pit lord", "shut up", "how so?", "hey now", "much appreciated",
+                      "i don't think so", "I know, right?", "it begins", "too soon", "well done",
+                      "i like it", "are you okay?", "ah, nice", "about time", "very good",
+                      "are you kidding me?", "at last", "got it", "what happened?", "oh boy",
+                      "nice one", "i am"]
