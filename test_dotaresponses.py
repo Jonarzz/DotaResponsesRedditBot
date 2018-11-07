6e088c21173f3b6ffbdd8e2bd5f9e808a6dc8695
Jonasz Czerepko <rajek7@gmail.com>
2018-11-07 20:35:52 +0100

    Test updates. Removed outdated tests. Fixed wrong Travis CI build (now actually runs tests). Fixes #24.

--- test_dotaresponses.py
+++ test_dotaresponses.py
@@ -13,34 +13,11 @@ class DotaResponsesTest(unittest.TestCase):
     """Class used to test dotaresponses module.
     Inherits from TestCase class of unittest module."""
 
-    def test_create_reply(self):
-        """Method that tests the create_reply method from dotaresponses module.
-
-        It checks whether the returned value is the same as the expected string.
-        """
-        responses_dict = parser.dictionary_from_file('test_responses_dict.txt')
-        heroes_dict = parser.dictionary_from_file('test_heroes_dict.txt')
-
-        expected_output = ("[{}]({}) (sound warning: {}){}"
-                           .format('abc', 'http://def.gh/Abad_a_.mp3', 'Abaddon',
-                                   properties.COMMENT_ENDING)
-                          )
-        self.assertEqual(dotaresponses.create_reply(responses_dict, heroes_dict, 'abc', 'abc'),
-                         expected_output)
-
-        expected_output = ("[{}]({}) (sound warning: {}){}"
-                           .format('123', 'http://456.78/Noba_a_.mp3', 'Techies',
-                                   properties.COMMENT_ENDING)
-                          )
-        self.assertEqual(dotaresponses.create_reply(responses_dict, heroes_dict, '123', '123'),
-                         expected_output)
-
     def test_prepare_response(self):
         """Method that tests the prepare_response method from dotaresponses module.
 
         It checks whether the returned value is the same as the expected string.
         """
-        self.assertEqual(dotaresponses.prepare_response("That's a great idea!!!"),
-                         "that's a great idea")
-        self.assertEqual(dotaresponses.prepare_response("Wonderfullll"), "wonderful")
+        self.assertEqual(dotaresponses.prepare_response("That's a great idea!!!"), "that's a great idea")
+        self.assertEqual(dotaresponses.prepare_response("  WoNdErFuL  "), "wonderful")
         self.assertEqual(dotaresponses.prepare_response("How are you?"), "how are you?")
