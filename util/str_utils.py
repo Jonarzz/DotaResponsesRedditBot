625e2c754e39226e83ff1895cda4e16c728fe7fe
MePsyDuck <sumit231996@gmail.com>
2019-12-04 11:38:26 +0530

    Update str_utils.py

--- util/str_utils.py
+++ util/str_utils.py
@@ -1,3 +1,4 @@
+import re
 import string
 
 PUNCTUATION_TRANS = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
@@ -7,20 +8,17 @@ WHITESPACE_TRANS = str.maketrans(string.whitespace, ' ' * len(string.whitespace)
 def preprocess_text(text):
     """Method for pre-processing the given response text.
     It:
-    * changes to lowercase
-    * removes trailing and leading spaces
     * removes all punctuations
     * removes double spaces
+    * changes to lowercase
+    * removes trailing and leading spaces
 
     :param text: the text to be cleaned
     :return: cleaned text
     """
-    text = text.strip().lower()
 
     text = text.translate(PUNCTUATION_TRANS)
     text = text.translate(WHITESPACE_TRANS)
-
-    while '  ' in text:
-        text = text.replace('  ', ' ')
-
+    text = re.sub(' +', ' ', text)
+    text = text.strip().lower()
     return text
