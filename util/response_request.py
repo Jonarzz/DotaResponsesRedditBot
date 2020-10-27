60c457340a9fc231d6bd8ad0387d2898d82ce3a1
choldener <choldener@users.noreply.github.com>
2020-10-27 04:03:17 -0500

    Removed excluded set in cargo request
     Removed Feature: Added unneeded complexity and decreased readbility.

--- util/response_request.py
+++ util/response_request.py
@@ -5,10 +5,7 @@ import requests
 def request_cargo_set(url):
     web_request = requests.get(url)
     web_json = web_request.json()
-    SELECTED_EXCLUDED_RESPONSES = {}
     cargo_set = set()
     for objects in web_json['cargoquery']:
-        cargo = preprocess_text(objects['title']['title'])
-        if cargo not in  SELECTED_EXCLUDED_RESPONSES: 
-            cargo_set.add(cargo)
+        cargo_set.add(preprocess_text(objects['title']['title']))
     return cargo_set
